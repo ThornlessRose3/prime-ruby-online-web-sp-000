324ee91eb1c5f310a33adfb81f97cd7279f65aaf
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-15 15:55:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,17 +6,17 @@ def prime?(num)
   when 2 # if num is 2, it is prime
     return false
   when num != 2 && num % 2 === 0 # if num is not 2, but even, it is NOT prime
-    return false
+    return true
   else # if num is 3 or greater, time to interate
     # interate over ranged array, dividing by 3 +2i. if mod i === 0, return false, unless i === num
   inc = 3
   (3 ..num).collect do |i| 
     if i % inc == 0 && inc == num
-      return false
+      return true
     else
       inc += 2
     end
   end
-  return true
+  return false
   end
 end
